2ccbf825cb5c12903959cb1becd798aecfe3ae31
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2021-11-29 14:35:42 +0530

    Initial project.

---
+++ spring.boot.uploadFile_backend/src/test/java/com/upload_file/spring/boot/uploadFile/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upload_file.spring.boot.uploadFile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
